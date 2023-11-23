d799a300a684bffb809adbefedfe1ec8552230aa
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2023-11-23 12:50:15 -0800

    Added a separate folder containing a separate script for the file organizer program where the path argument is passed as an argument in the command-line.

---
+++ flie_organizer_passing_arguments_in_the_command_line/README.md
@@ -0,0 +1,63 @@
+# Python File Organizer.
+This command-line python file organizer is a program that
+scans a directory for files and creates folders for all
+the file extensions where each folder bares the name 
+of an extension and files in the directory are moved into
+these folders according to their extension.
+
+
+## Features of the file organizer
+1. **Takes path as a command-line argument**: The program takes the path(which must be embedded in quotes("")) to
+the directory whose contents are to be organized through the command-line. It organizes
+the files in the directory if the path is valid or returns an error message if an error occurs. 
+2. **Extracts extension and creates folders**: The program 
+extracts the extension of the files in the directory and 
+creates a folder for each extension where files of the 
+same type are stored.
+3. **Organize files**: The extension of each file in the 
+directory is checked against the folder names in the directory,
+and if the extension matches the name of the folder, the
+file is moved into the folder.
+
+## Getting started
+1. **Clone the repository to your machine.**
+```shell
+git clone https://github.com/EmmanuelBronyah/Python-File-Organizer.git
+```
+2. **Navigate to the project directory.**
+```shell
+cd file-organizer
+```
+3. **Run the program.**
+```shell
+python file_organizer_cli.py
+```
+
+## Usage
+* You would need to add the path as argument in the 
+command-line when you run the script. The program
+outputs the message 'Files organized.' when it is done 
+executing, or otherwise outputs a different message 
+pertaining to an error that might have occurred.
+Please note that the path to the folder must be placed in quotes("").
+And also folder names with spaces would require the path
+to be embedded in quotes("").
+
+## Example
+1. Adding a path argument.
+```shell
+(venv) PS C:\Users\{User}\PycharmProjects\file-organizer> python file_organizer_cli "C:\Users\ManuelB\Desktop\File-arranger-folder"
+Files organized.
+```
+2. Adding a path argument not embedded in quotes("") where the folder name has spaces.
+```shell
+(venv) PS C:\Users\{User}\PycharmProjects\file-organizer> python file_organizer_cli C:\Users\ManuelB\Desktop\File arranger folder
+usage: File organizer [-h] [P]
+File organizer: error: unrecognized arguments: arranger folder
+```
+
+# License
+This project is licensed under the MIT license.
+
+# Acknowledgements
+* Built by Bronyah Emmanuel.
---
+++ flie_organizer_passing_arguments_in_the_command_line/file_organizer_cli.py
@@ -0,0 +1,105 @@
+import os
+import argparse
+import shutil
+
+
+def main():
+    args = arg_parser()
+    path = path_validation(args)
+    if path is not None and path != -1:
+        extensions = get_extensions(args)
+        create_directories(args, extensions)
+        organize_files(args)
+    else:
+        if path == -1:
+            print('Program ended.')
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser(
+        prog='File organizer',
+        description='Takes a valid path and groups files based on their extensions.',
+        epilog='The program scans a directory for files and creates folders for all \
+                the file extensions where each folder bares the name \
+                of an extension and files in the directory are moved into \
+                these folders according to their extension.'
+    )
+    parser.add_argument('path', metavar='P', type=str, nargs='?', help='Path to the folder.')
+    args = parser.parse_args()
+    args = args.path
+    return args
+
+
+def path_validation(path):
+    """
+    Takes as input the path to the folder to be organized.
+
+    If no path is provided, -1 is returned and the program ends.
+    Returns None if an error occurs and prints to the screen the error message.
+    """
+    path = path or 'exit'
+    if path == 'exit':
+        return -1
+    try:
+        if os.path.isfile(path):
+            raise NotADirectoryError(f'The path "{path}" points to a file, not a folder.')
+        elif os.path.isdir(path):
+            return path
+        else:
+            print(f'The path {path} is invalid. Enter a valid path.')
+            return None
+    except NotADirectoryError as inst:
+        print(inst)
+        return None
+
+
+def get_extensions(path):
+    """
+    Generates the extension of files in the current path.
+
+    :param path: This is a string representing the current file path.
+    :return: Returns a list of unique extensions from which folders will be created.
+    """
+    extensions = []
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            extension = os.path.splitext(file)[1]
+            if extension not in extensions:
+                extensions.append(extension)
+    return extensions
+
+
+def create_directories(path, extensions):
+    """
+    Creates folders of the list of extensions.
+
+    :param path: This is a string representing the current file path.
+    :param extensions: This is a list of unique extensions from which folders will be created.
+    :return: Returns None.
+    """
+    os.chdir(path)
+    for ext in extensions:
+        os.makedirs(ext, exist_ok=True)
+
+
+def organize_files(path):
+    """
+    Moves files into folders where the file extension is the same as the folder name.
+
+    :param path: This is a string representing the current file path.
+    :return: Returns None.
+    """
+    for item in os.listdir(path):  # item could be a file or folder.
+        if os.path.isdir(item):
+            for obj in os.listdir(path):  # obj could be a file or folder.
+                if os.path.isfile(obj):
+                    file_extension = os.path.splitext(obj)[1]
+                    if file_extension == item:
+                        old_path = os.path.join(path, obj)
+                        new_path = os.path.join(os.path.join(path, item), obj)
+                        shutil.move(old_path, new_path)
+    print('Files organized.')
+
+
+if __name__ == '__main__':
+    main()
